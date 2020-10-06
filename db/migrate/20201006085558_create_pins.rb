2e725dafdbde258a9106ef072befe2daaf6f1364
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-06 14:21:19 +0300

    Pins CRUD Ability

---
+++ db/migrate/20201006085558_create_pins.rb
@@ -0,0 +1,10 @@
+class CreatePins < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pins do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
